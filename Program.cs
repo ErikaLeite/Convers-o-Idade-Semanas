65b31e1e2e89e5df11f7e6aa75013f8c5949bc8b
ERIKA LEITE PAIVA <you@example.com>
2020-11-13 11:07:07 -0300

    Projeo Iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Convers_o_Idade_Semanas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
